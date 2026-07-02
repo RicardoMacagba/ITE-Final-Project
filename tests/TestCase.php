f06f9e00095b7318133e4fa55fa8a369ea4c4525
RicardoMacagba <155034677+RicardoMacagba@users.noreply.github.com>
2026-07-02 23:34:34 +0800

    fixed
    fix the failure notification

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,27 @@
 <?php
 
+// namespace Tests;
+
+// use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+// abstract class TestCase extends BaseTestCase
+// {
+//     //
+// }
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // This prevents Laravel from looking for the manifest.json file during tests
+        $this->withoutVite(); 
+    }
 }
